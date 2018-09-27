7d7956b822c723096c8e3396da9069746745b17a
Bruno Lima <brunolimamarq@gmail.com>
2018-09-27 16:01:59 -0300

    saving last date on restartapp

--- Sources/TrueTime.swift
+++ Sources/TrueTime.swift
@@ -24,20 +24,25 @@ public final class ReferenceTime: NSObject {
     @objc public var uptimeInterval: TimeInterval { return underlyingValue.uptimeInterval }
     @objc public var time: Date { return underlyingValue.time }
     @objc public var uptime: timeval { return underlyingValue.uptime }
-    @objc public func now() -> Date { return underlyingValue.now() }
-
+    @objc public func now() -> Date {
+        let now = underlyingValue.now()
+        UserDefaultUtil.instance.save(date: underlyingValue.time, uptime: uptime)
+        
+        return now
+    }
+    
     public convenience init(time: Date, uptime: timeval) {
         self.init(FrozenReferenceTime(time: time, uptime: uptime))
     }
-
+    
     init(_ underlyingValue: FrozenTime) {
         self.underlyingValueLock = GCDLock(value: underlyingValue)
     }
-
+    
     public override var description: String {
         return "\(type(of: self))(underlyingValue: \(underlyingValue)"
     }
-
+    
     private let underlyingValueLock: GCDLock<FrozenTime>
     var underlyingValue: FrozenTime {
         get { return underlyingValueLock.read() }
@@ -65,36 +70,39 @@ public typealias LogCallback = (String) -> Void
                            pollInterval: pollInterval)
         ntp = NTPClient(config: config)
     }
-
+    
     @objc public func start(pool: [String] = ["time.apple.com"], port: Int = 123) {
         ntp.start(pool: pool, port: port)
     }
-
+    
     @objc public func pause() {
         ntp.pause()
     }
-
+    
     public func fetchIfNeeded(queue callbackQueue: DispatchQueue = .main,
                               first: ReferenceTimeCallback? = nil,
                               completion: ReferenceTimeCallback? = nil) {
         ntp.fetchIfNeeded(queue: callbackQueue, first: first, completion: completion)
     }
-
-#if DEBUG_LOGGING
+    
+    #if DEBUG_LOGGING
     @objc public var logCallback: LogCallback? = defaultLogger {
         didSet {
             ntp.logger = logCallback
         }
     }
-#endif
-
-    @objc public var referenceTime: ReferenceTime? { return ntp.referenceTime }
+    #endif
+    
+    @objc public var referenceTime: ReferenceTime? {
+        return fetchReferenceTime()
+        
+    }
     @objc public var timeout: TimeInterval { return config.timeout }
     @objc public var maxRetries: Int { return config.maxRetries }
     @objc public var maxConnections: Int { return config.maxConnections }
     @objc public var maxServers: Int { return config.maxServers}
     @objc public var numberOfSamples: Int { return config.numberOfSamples}
-
+    
     private let config: NTPConfig
     private let ntp: NTPClient
 }
@@ -103,11 +111,11 @@ extension TrueTimeClient {
     @objc public func fetchFirstIfNeeded(success: @escaping (ReferenceTime) -> Void, failure: ((NSError) -> Void)?) {
         fetchFirstIfNeeded(success: success, failure: failure, onQueue: .main)
     }
-
+    
     @objc public func fetchIfNeeded(success: @escaping (ReferenceTime) -> Void, failure: ((NSError) -> Void)?) {
         fetchIfNeeded(success: success, failure: failure, onQueue: .main)
     }
-
+    
     @objc public func fetchFirstIfNeeded(success: @escaping (ReferenceTime) -> Void,
                                          failure: ((NSError) -> Void)?,
                                          onQueue queue: DispatchQueue) {
@@ -115,7 +123,7 @@ extension TrueTimeClient {
             self.mapBridgedResult(result, success: success, failure: failure)
         })
     }
-
+    
     @objc public func fetchIfNeeded(success: @escaping (ReferenceTime) -> Void,
                                     failure: ((NSError) -> Void)?,
                                     onQueue queue: DispatchQueue) {
@@ -123,12 +131,93 @@ extension TrueTimeClient {
             self.mapBridgedResult(result, success: success, failure: failure)
         }
     }
-
+    
     private func mapBridgedResult(_ result: ReferenceTimeResult,
                                   success: (ReferenceTime) -> Void,
                                   failure: ((NSError) -> Void)?) {
         result.analysis(ifSuccess: success, ifFailure: { err in failure?(err) })
     }
+    
+    fileprivate func fetchReferenceTime() -> ReferenceTime? {
+        var referenceTime: ReferenceTime?
+        
+        if let reference = ntp.referenceTime {
+            referenceTime = reference
+        }
+        else {
+            let userDefaults    = UserDefaultUtil.instance
+            let (date, uptime)  = userDefaults.loadSavedDate()
+
+            if let uptime = uptime {
+                if uptime.milliseconds > timeval.uptime().milliseconds {
+                    userDefaults.clearValues()
+                }
+                else {
+                    referenceTime = ReferenceTime(time: date, uptime: uptime)
+                }
+            }
+        }
+        
+        return referenceTime
+    }
 }
 
 let defaultLogger: LogCallback = { print($0) }
+
+
+class UserDefaultUtil {
+    
+    let userDefaults        = UserDefaults.standard
+    let dateValue           = "dateValue"
+    let secUptime           = "secUptime"
+    let milisecUptime       = "milisecUptime"
+    
+    private init() {}
+    private static var _instance: UserDefaultUtil?
+    
+    static var instance: UserDefaultUtil {
+        if _instance == nil { _instance = UserDefaultUtil() }
+        return _instance!
+    }
+    
+    
+    func save(date: Date, uptime: timeval) {
+        userDefaults.set(date.timeIntervalSince1970 as Double, forKey: dateValue)
+        userDefaults.set(uptime.tv_sec, forKey: secUptime)
+        userDefaults.set(uptime.tv_usec, forKey: milisecUptime)
+    }
+    
+    func clearValues() {
+        userDefaults.set(nil, forKey: dateValue)
+        userDefaults.set(nil, forKey: secUptime)
+        userDefaults.set(nil, forKey: milisecUptime)
+    }
+    
+    func loadSavedDate() -> (Date, timeval?) {
+        let timeInterval    = userDefaults.double(forKey: dateValue)
+        let date            = Date(timeIntervalSince1970: timeInterval)
+        
+        let secTimeValue        = userDefaults.value(forKey: secUptime) as? Int
+        let milisecTimeValue    = userDefaults.value(forKey: milisecUptime) as? Int32
+        
+        
+        var timeValue: timeval?
+        if let sec = secTimeValue, let milisec = milisecTimeValue {
+            timeValue   = timeval(tv_sec: sec, tv_usec: milisec)
+        }
+        
+        return (date, timeValue)
+    }
+    
+    
+}
+
+extension Date {
+    
+    mutating func add(_ value: Int, to component: Calendar.Component) {
+        if let date = Calendar.current.date(byAdding: component, value: value, to: self) {
+            self = date
+        }
+    }
+    
+}
